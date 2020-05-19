6adabd4d48f3c98d375013052d34ef8b6b7c301c
kjh <ex-itkjh@ex-it.co.kr>
2020-05-19 10:30:28 +0900

    홈페이지 세팅

---
+++ pages/web/main.vue
@@ -0,0 +1,224 @@
+<template>
+  <div class="wrapper">
+    <div class="skip-navi">
+      <a href="#container">본문 바로가기</a>
+      <a href="#gnb">메뉴 바로가기</a>
+    </div>
+    <div class="wrap main">
+      <header class="w-txt">
+        <div class="xt-ci">
+          <ul class="gnb-menu">
+            <li class="gnb-menu01">Who we are</li>
+            <li class="gnb-menu02">We make</li>
+            <li class="gnb-menu03">XTSIEN Life</li>
+            <li class="gnb-menu04">Contact XT</li>
+          </ul>
+        </div>
+        <div class="pdf-down">Start a project x Download pdf</div>
+        <div class="hamburger">
+          <a href="#">
+            <div class="side-open">
+              <span class="m">M</span>
+              <span class="e">E</span>
+              <span class="n">N</span>
+              <span class="u">U</span>
+            </div>
+            <div class="side-close">
+              <span></span>
+              <span></span>
+            </div>
+          </a>
+        </div>
+        <nav>
+          <ul id="gnb">
+            <li><a href="#">XT</a></li>
+            <li><a href="#">Works</a></li>
+            <li><a href="#">XTSIEN Life</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">News</a></li>
+          </ul>
+          <div class="profile-down">
+            <a href="#">Download XT profile pdf</a>
+          </div>
+        </nav>
+      </header>
+      <div class="container">
+        <div class="main-content" id="pagepiling">
+          <section class="main-section" id="section1">
+            <video autoplay loop muted id="myVideo">
+              <source src="../../assets/web/mov/flowers.mp4" type="video/mp4">
+              <!--<source src="../../assets/web/mov/flowers.webm" type="video/webm">-->
+            </video>
+            <div class="layer">
+              <img src="../../assets/web/images/main/xt-exp.png" alt="xt eXperience">
+            </div>
+          </section>
+
+          <!-- Who we are -->
+          <section class="main-section" id="section2">
+            <div class="swiper-container">
+              <div class="swiper-wrapper">
+                <div class="swiper-slide we-are01">
+                  <div class="weAre-txt">
+                    <h2>GROWING<br>XT <span>Expert X Empathetic</span></h2>
+                    <div>우리는 당신 ; 고객을 가장 잘 알고 싶습니다<br>
+                    열린사고로 미리 판단하지 않고<br>
+                    당신 ; 고객의 입장에서 생각하고 공감하며<br>
+                    가장 당신 ; 고객다운 모습을 집요하게 분석하고 ​<br>
+                    이성적인 분석을 바탕으로 창의적이면서도<br>
+                    논리적인 UIUX 설계합니다 </div>
+                  </div>
+                </div>
+                <div class="swiper-slide we-are02">
+                  <div class="weAre-txt">
+                    <h2>GROWING<br>XT <span>Growth X Flexible</span></h2>
+                    <div>우리는 새로운 것에 유연합니다.<br>
+                    ​새로운 것을 유연하게 만나고 경험하여<br>
+                    많은 것을 알게 되고 자라지만<br>
+                    우리에 시선은 항상 새로운 곳을 향하여 있어<br>
+                    멈추지 않고 ​또 다시 지속적으로 탐구하고<br>
+                    그려 냅니다</div>
+                  </div>
+                </div>
+                <div class="swiper-slide we-are03">
+                  <div class="weAre-txt">
+                    <h2>GROWING<br>XT <span>Expert X Empathetic</span></h2>
+                    <div>우리는 당신 ; 고객을 가장 잘 알고 싶습니다<br>
+                    열린사고로 미리 판단하지 않고<br>
+                    당신 ; 고객의 입장에서 생각하고 공감하며<br>
+                    가장 당신 ; 고객다운 모습을 집요하게 분석하고 ​<br>
+                    이성적인 분석을 바탕으로 창의적이면서도<br>
+                    논리적인 UIUX 설계합니다 </div>
+                  </div>
+                </div>
+                <div class="swiper-slide we-are04">
+                  <div class="weAre-txt">
+                    <h2>GROWING<br>XT <span>Growth X Flexible</span></h2>
+                    <div>우리는 새로운 것에 유연합니다.<br>
+                    ​새로운 것을 유연하게 만나고 경험하여<br>
+                    많은 것을 알게 되고 자라지만<br>
+                    우리에 시선은 항상 새로운 곳을 향하여 있어<br>
+                    멈추지 않고 ​또 다시 지속적으로 탐구하고<br>
+                    그려 냅니다</div>
+                  </div>
+                </div>
+                <div class="swiper-slide we-are05">
+                  <div class="weAre-txt">
+                    <h2>GROWING<br>XT <span>Expert X Empathetic</span></h2>
+                    <div>우리는 당신 ; 고객을 가장 잘 알고 싶습니다<br>
+                    열린사고로 미리 판단하지 않고<br>
+                    당신 ; 고객의 입장에서 생각하고 공감하며<br>
+                    가장 당신 ; 고객다운 모습을 집요하게 분석하고 ​<br>
+                    이성적인 분석을 바탕으로 창의적이면서도<br>
+                    논리적인 UIUX 설계합니다 </div>
+                  </div>
+                </div>
+              </div>
+              <div class="swiper-pagination"></div>
+            </div>
+           </section>
+          <!-- // Who we are -->
+
+          <!-- We make -->
+          <section class="main-section" id="section3">
+            <div class="portfolio">
+              <div>
+                <a href="#"><img src="../../assets/web/images/main/pj-wellihilli.png"></a>
+                <p><span>2020</span>웰리힐리파크 통합사이트 구축</p>
+              </div>
+              <div>
+                <a href="#"><img src="../../assets/web/images/main/pj-kb.png"></a>
+                <p><span>2019</span>KB캐피탈 KB차차차3.0 리뉴얼</p>
+              </div>
+              <div>
+                <a href="#"><img src="../../assets/web/images/main/pj-resorts.png"></a>
+                <p><span>2020</span>한화리조트 모바일앱 리뉴얼</p>
+              </div>
+              <div>
+                <a href="#"><img src="../../assets/web/images/main/pj-hanwha.png"></a>
+                <p><span>2020</span>웰리힐리파크 통합사이트 구축</p>
+              </div>
+            </div>
+            <p><a href="#">MORE PROJECTS</a></p>
+          </section>
+          <!-- // We make -->
+
+          <!-- XTSIEN Life -->
+          <section class="main-section" id="section4">
+            <span class="life bar-txt">Stable X Pleasure</span>
+            <div class="life-txt">
+              <span class="bar-txt">우리는 즐거워서 <span class="line-txt">오래 다니고 싶은 회사</span>를 지향합니다</span><br>
+              <span class="bar-txt">다양한 우리들이 만나 즐겁고 또 각자가 따로 있어 즐겁고</span><br>
+              <span class="bar-txt">그렇게 우리는 <span class="line-txt">함께 또 따로</span> 즐거운</span><br>
+              <span class="bar-txt">엑스티를 만들어 갑니다</span></div>
+            <p class="life-btn"><a href="#" class="bar-txt">eXperience XTSIEN Life</a></p>
+          </section>
+          <!-- // XTSIEN Life -->
+
+          <!-- Contact XT -->
+          <section class="main-section" id="section5">
+            <div>당신 다움을 함께 탐험할<br>엑스티가 당신을 기다리고 있어요</div>
+            <p><a href="#">Contact XT</a></p>
+            <div>
+              <a href="#">Project Request</a>
+              <a href="#">Career Apply</a>
+            </div>
+          </section>
+          <!-- // Contact XT -->
+        </div>
+      </div>
+      <footer>
+        <p class="copy">© 2020 XT Inc.Rights Reserved.</p>
+        <p class="contact">
+          <span>T +82 2 853 6582</span>
+          <span>F  +82 2 853 6582</span>
+        </p>
+        <a href="#">
+          <span class="t">T</span>
+          <span class="o">O</span>
+          <span class="p">P</span>
+        </a>
+      </footer>
+    </div>
+    <div id="mask"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    head() {
+      return {
+        script : [
+          {src: "/js/jquery.pagepiling.js"},
+          {src: "/js/swiper.js"},
+          {src: "/js/common.js"}
+        ]
+      }
+    },
+    mounted() {
+      $('#pagepiling').pagepiling({
+
+      });
+      var swiper = new Swiper('.swiper-container', {
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'fraction',
+        },
+        /*navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },*/
+      });
+      $(window).resize(function () {
+        $(".swiper-slide, .swiper-pagination").css("width", $(window).width());
+      });
+      $(".swiper-pagination").css("width", "40px");
+    }
+    /*methods: {
+
+    }*/
+
+  }
+
+
+</script>
